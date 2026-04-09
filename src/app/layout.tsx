e8d1e4704d066d81548aa612aa51bf61a8bfa08c
Mars Ponce <mars@marsponce.ca>
2026-04-09 18:25:44 -0400

    chore: fix metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import '@/styles/globals.css';
 
 export const metadata: Metadata = {
-  title: 'tobytran.art',
-  description: "Toby Tran's artist site",
+  title: 'julessansom.com',
+  description: "Jules Sansom's artist site",
 };
 
 export default function RootLayout({
